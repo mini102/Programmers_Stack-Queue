b5541d87275e1db444d8ab575f2364a075c2791c
mini102 <73246476+mini102@users.noreply.github.com>
2022-03-16 09:49:41 +0900

    Create A4_modified.py
    효율성은 아직 통과 못함

---
+++ A4_modified.py
@@ -0,0 +1,23 @@
+def solution(prices):
+    answer = [0]*len(prices)
+    stack = [(idx+1,i) for idx,i in enumerate(prices)]
+    #print(stack)
+    while len(stack)>0:
+        clist = stack.copy()
+        top = stack.pop(0)
+        # print("top:{}".format(top))
+        clist.sort(key=lambda x: x[1])
+        # if clist[0][1]<top[1]:
+        if clist[0][1]==top[1]:
+                answer[top[0]-1] = len(stack)
+                continue
+        for idx,p in enumerate(stack):
+            # print("idx: {}, p:{}".format(idx,p))
+            if p[1]<top[1]:
+                ans = p[0]-top[0]
+                answer[top[0]-1]=ans 
+                break
+            # if idx == len(stack)-1:
+            #     answer[top[0]-1] = len(stack)
+    
+    return answer
